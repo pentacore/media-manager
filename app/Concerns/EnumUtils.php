16610695af484c30681cd62944e28e49cedd7a01
Martin Claesson <martin.claesson@waya.se>
2026-04-13 12:25:14 +0200

    docs: document enum utils

--- app/Concerns/EnumUtils.php
+++ app/Concerns/EnumUtils.php
@@ -9,21 +9,36 @@ use Illuminate\Validation\Rules\In;
 
 trait EnumUtils
 {
+    /**
+     * Returns an array of the enum case names.
+     * @return array<int, string>
+     */
     public static function names(): array
     {
         return array_column(self::cases(), 'name');
     }
 
+    /**
+     * Returns an array of the enum values.
+     * @return array<int, int|string>
+     */
     public static function values(): array
     {
         return array_column(self::cases(), 'value');
     }
 
+    /**
+     * Returns an associative array of enum values as keys and their corresponding names as values.
+     * @return array<int|string, string>
+     */
     public static function array(): array
     {
         return array_combine(self::values(), self::names());
     }
 
+    /**
+     * Returns a validation rule that checks if a value is one of the enum values.
+     */
     public static function validationRule(): In
     {
         return Rule::in(self::values());
@@ -39,11 +54,20 @@ trait EnumUtils
         }
     }
 
+    /**
+     * Converts the enum value to a URL-friendly slug.
+     */
     public function asSlug($separator = '-', $language = 'en', $dictionary = ['@' => 'at']): string
     {
         return new Stringable($this->value)->slug($separator, $language, $dictionary)->toString();
     }
 
+    /**
+     * Returns an array of enum values and their corresponding names for use in a select dropdown.
+     * If $withNull is true, the array will include a null value at index 0.
+     *
+     * @return array<int, array{name: string, value: int|string}>
+     */
     public static function mapForSelect(bool $withNull = false): array
     {
         $arr = [];
@@ -57,11 +81,17 @@ trait EnumUtils
         return array_merge($arr, $values);
     }
 
+    /**
+     * Returns a comma-separated string of enum values.
+     */
     public static function commaSeparatedValues(): string
     {
         return implode(',', self::values());
     }
 
+    /**
+     * returns the enum value as a string
+     */
     public function toString(): string
     {
         return (string) $this->value;
