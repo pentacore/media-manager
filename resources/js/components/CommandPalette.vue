71d701bcfe04edd86177c3ab671d58d90120201a
Martin Claesson <martin.claesson@waya.se>
2026-07-04 15:07:55 +0200

    npm lint

--- resources/js/components/CommandPalette.vue
+++ resources/js/components/CommandPalette.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { router } from '@inertiajs/vue3';
-import { useEventListener } from '@vueuse/core';
 import {
     Activity as ActivityIcon,
     Film,
@@ -14,6 +13,7 @@ import {
     Tv,
     Zap,
 } from '@lucide/vue';
+import { useEventListener } from '@vueuse/core';
 import type { Component } from 'vue';
 import { computed, nextTick, onMounted, useTemplateRef, watch } from 'vue';
 import ActionRequestController from '@/actions/App/Http/Controllers/Actions/ActionRequestController';
